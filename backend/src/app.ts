4bccaedb4a00db3fcc5676c6aa166cadfc1d9f94
kratiBXA <krati.gupta@bounteous.com>
2025-09-20 00:14:17 +0530

    updated files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,11 @@ import cartRouter from './routes/cartRoute.js';
 import authRouter  from './routes/authRoute.js';
 
 const app = express();
+<<<<<<< HEAD
 app.use(cors({origin :'http://localhost:5173', credentials: true}));
+=======
+app.use(cors({origin: 'http://localhost:5173',credentials: true}));
+>>>>>>> 95dc36cb5e18f3f73c263fc661cef1c52a805c2c
 app.use(express.json());
 app.use(cookieParser());
 
