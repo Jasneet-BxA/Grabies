035fef5c2228af827ec32841db0f20b963e3ffab
Jasneet-BxA <Jasneet.Dhiman@bounteous.com>
2025-09-24 11:58:38 +0530

    added checkout functionality

--- backend/src/config/env/index.ts
+++ backend/src/config/env/index.ts
@@ -11,6 +11,7 @@ export const env = createEnv({
     PORT: z.coerce.number().default(3000),
     JWT_EXPIRES_IN: z.string().default('1d'),
     SUPABASE_KEY: z.string().optional(),
+    STRIPE_SECRET_KEY: z.string().min(10), 
   },
   runtimeEnv: process.env,
 });
