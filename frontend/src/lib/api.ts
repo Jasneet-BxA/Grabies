f5fcad67f0657ef198878016fc9a2a1492598d75
Jasneet-BxA <Jasneet.Dhiman@bounteous.com>
2025-09-26 00:09:51 +0530

    updated files

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_BACKEND_URL,
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 })
 // Authentication
